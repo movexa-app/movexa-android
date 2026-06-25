8fb46d93f5570ef6712a470b773cb208e9f249b7
Anmol Trivedi <anmoltrvd01@gmail.com>
2026-06-25 19:34:47 +0530

    wrap NavGraph in Box with innerPadding to fix gesture offset behind nav bar

--- app/src/main/java/com/example/movexa_android/MainActivity.kt
+++ app/src/main/java/com/example/movexa_android/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
@@ -32,9 +33,11 @@ class MainActivity : ComponentActivity() {
                         MovexaBottomNav(navController = navController)
                     }
                 ) { innerPadding ->
-                    MovexaNavGraph(
-                        navController = navController,
-                    )
+                    androidx.compose.foundation.layout.Box(modifier = Modifier.padding(innerPadding)) {
+                        MovexaNavGraph(
+                            navController = navController,
+                        )
+                    }
                 }
             }
         }
